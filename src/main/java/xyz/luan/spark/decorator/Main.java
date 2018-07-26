5d34e1a1994c60590481ae30775c387ad7b3b748
Luan Nico <luanpotter27@gmail.com>
2018-07-26 17:08:38 -0300

    First commit (using xyz-generator)

---
+++ src/main/java/xyz/luan/spark/decorator/Main.java
@@ -0,0 +1,7 @@
+package xyz.luan.spark.decorator;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
---
+++ src/test/java/xyz/luan/spark/decorator/MainTest.java
@@ -0,0 +1,11 @@
+package xyz.luan.spark.decorator;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class MainTest {
+	@Test
+	public void simpleTest() {
+		assertThat(2 + 2).isEqualTo(4);
+	}
+}
